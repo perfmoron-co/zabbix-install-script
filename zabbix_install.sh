de782aa427e4ff194b96ccd2b929e62c2b77002e
Vipul Kane-admin <vkane-admin@WDNZ-DUR3013-SE.corp.wynyardgroup.com>
2016-05-16 11:26:54 +1200

    Fixing string comparison, donload links and some typos

--- zabbix_install.sh
+++ zabbix_install.sh
@@ -13,7 +13,7 @@
 #Set Default script variables here before running
 DATABASE="mysql" # option ONLY mysql at present
 IPv6=true # options true or false
-VERSION="2.4.7"
+VERSION="3.0.2"
 DB_USER="zabbix"
 DB_PASS="zabb1x"
 DB_HOST="localhost"
@@ -26,7 +26,7 @@ SERVER_INSTALL=true # if false assumed to be agent only install
 #echo "$IPv6"
 #echo "$VERSION"
 #echo "$DB_USER"
-echo "$DB_PASS"
+#echo "$DB_PASS"
 #echo "$DB_HOST"
 #echo "$SERVER_IP"
 #echo "$SERVER_INSTALL"
@@ -73,7 +73,7 @@ if [ -n "$t1" ]
 then
   VERSION="$t1"
 else
-  VERSION="2.4.7"
+  VERSION="3.0.2"
 fi
 #====END Get_Version
  
@@ -154,13 +154,10 @@ else
    exit 1
 fi
 
-DATABASE="mysql"
-echo "database :$DATABASE"
-
 if $SERVER_INSTALL ; then
 
 echo '**********Test1***************'
-   if [ $DATABASE == "mysql" ]; then
+   if [ $DATABASE = "mysql" ]; then
       echo -n "   Input the MySQL admin user name: "
       read MySQLADMIN
 
@@ -331,14 +328,13 @@ cat <<EOF > /etc/apache2/sites-available/zabbix
                 	Options FollowSymLinks Indexes MultiViews
 	                AllowOverride None
         	</Directory>
-p
 	</VirtualHost>
 EOF
 
    echo "   Zabbix $VERSION installation"
 fi
 
-#step 5 for agents only they join here…
+#step 5 for agents only they join here
 
 ln -s /usr/bin/fping /usr/sbin/fping
 if $IPv6 ; then
